c8428d814f97355a3d08926ecc50e5b0514253ab
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-05-29 00:28:50 +0700

    adding vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import GlobalStyles from './styles/GlobalStyle.tsx'
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyles/>
