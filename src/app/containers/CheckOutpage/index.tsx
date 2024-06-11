38b9a68760f5a2e143a090b620feda62c972172f
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-06-11 16:21:34 +0700

    discard unused

--- src/app/containers/CheckOutpage/index.tsx
+++ src/app/containers/CheckOutpage/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, redirect, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Marginer } from "../../components/marginer";
 import axios from 'axios';
 import Checkbox from '@mui/material/Checkbox';
