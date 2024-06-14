68c0b8bff537b67159ea8dace6462619965f7ddc
duyhuynhnguyentuan <andyhntd2003@gmail.com>
2024-06-14 17:49:42 +0700

    cors

--- server/index.js
+++ server/index.js
@@ -3,22 +3,27 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import Bill from '../../../../email/Bill'; // Adjust this path as needed
+import path from 'path';
+import Bill from '../email/Bill'; // Adjust the path as needed
 import Resend from 'resend'; // Make sure this is the correct import for resend
 
-const resend = new Resend(process.env.VITE_APP_RESEND_API_KEY); // Ensure environment variable is set
+const resend = new Resend(process.env.VITE_APP_RESEND_API_KEY);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// CORS configuration
+const corsOptions = {
+  origin: 'https://vietafood.shop',
+  optionsSuccessStatus: 200 // For legacy browser support
+};
 
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200,
-  }
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.post('/send-email', async (req, res) => {
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+
+app.post('/api/send-email', async (req, res) => {
   try {
     const { order } = req.body;
     const emailContent = ReactDOMServer.renderToString(<Bill order={order} />);
@@ -35,6 +40,11 @@ app.post('/send-email', async (req, res) => {
     console.error('Error sending email:', error);
     res.status(500).send('Error sending email');
   }
+});
+
+// All other requests not handled will serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
